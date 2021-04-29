65c074ce2229a3d51e1f9b85f97be6f0bb85345a
Milan Jovanovic (Akvelon INC) <v-miljov@microsoft.com>
2021-04-29 16:09:21 +0200

    second push

---
+++ TestRunnerApp/TestRunnerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRunnerAppApp.swift
+//  TestRunnerApp
+//
+//  Created by Admin on 29.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestRunnerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
