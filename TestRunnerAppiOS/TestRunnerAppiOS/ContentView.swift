79b07b0702841085cb8c5ac1e234be984453a1eb
gordan-radojcic <69194114+gordan-radojcic@users.noreply.github.com>
2021-05-11 14:35:34 +0300

    New Code Version

---
+++ TestRunnerAppiOS/TestRunnerAppiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestRunnerAppiOS
+//
+//  Created by Admin on 11.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestRunnerAppiOS/TestRunnerAppiOS/TestRunnerAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRunnerAppiOSApp.swift
+//  TestRunnerAppiOS
+//
+//  Created by Admin on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestRunnerAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
